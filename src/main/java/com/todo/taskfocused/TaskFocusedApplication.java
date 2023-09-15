0b6be608cbc8ada0390a854e023081a9ad32895d
Diego Acosta <diemauacro@gmail.com>
2023-09-15 02:45:04 -0500

    Create spring project for task-focused

---
+++ src/main/java/com/todo/taskfocused/TaskFocusedApplication.java
@@ -0,0 +1,13 @@
+package com.todo.taskfocused;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFocusedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFocusedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo/taskfocused/TaskFocusedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.taskfocused;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskFocusedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
